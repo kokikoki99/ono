99ab1512a084d3bae12633a2e70586c4d726f8f5
kokikoki99 <144980907+kokikoki99@users.noreply.github.com>
2023-09-21 04:34:41 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import random
-if st.title('おみくじアプリ')
+st.title('おみくじアプリ')
 if st.button("おみくじを引く"):
     results=["大吉","忠吉","小吉","吉","凶","大凶"]
     result=random.choice(results)
